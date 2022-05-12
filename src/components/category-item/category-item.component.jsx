6fe196f84eff32b3f785aeb9213a347adf150e45
Sergio Morales <chemoral87@hotmail.com>
2022-05-12 09:44:22 -0500

    updating directory

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,19 @@
+import "./category-item.style.scss";
+
+const CategoryItem = ({ category }) => {
+  const { id, imageUrl, title } = category;
+  return (
+    <div key={id} className="category-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl}` }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Show Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
